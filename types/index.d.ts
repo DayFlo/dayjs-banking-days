4045a25c231a61a5a9f83dec2d7e149ff3e40b20
dayflo <dayton.floyd@extra.app>
2024-02-07 00:19:13 -0500

    Update: package and tsconfig updates + Add ESM support + Add CJS support + Include test directory

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+import dayjs from 'dayjs'
+
+type occurrence = -1 | 1 | 2 | 3 | 4 | 5
+type dayOfWeek = dayjs.Dayjs['day'] // 0-6
+// fixed date holidays are in the format 'MM-DD'
+interface HolidayMap {
+	fixedDateHolidays: string[]
+	floatingDateHolidays: {
+		[key: string]: [dayOfWeek, occurrence]
+	}
+}
+
+declare module 'dayjs' {
+	interface Dayjs {
+		isBankingDay(): boolean
+		isBankHoliday(): boolean
+		addBankingDays(): Dayjs
+		subtractBankingDays(): Dayjs
+		nextBankingDay(): Dayjs
+		previousBankingDay(): Dayjs
+	}
+}
+
+export { HolidayMap }
